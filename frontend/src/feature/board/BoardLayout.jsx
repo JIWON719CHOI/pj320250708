66547224dd667827d02f6191439101c0dc8f93a4
Jiwon Choi <jiwon719choi@gmail.com>
2025-07-10 15:10:23 +0900

    앱 푸터 만듦

--- frontend/src/feature/board/BoardLayout.jsx
+++ frontend/src/feature/board/BoardLayout.jsx
@@ -1,10 +1,8 @@
-import { Carousel } from "react-bootstrap";
+import { Accordion, Carousel, Tab, Tabs } from "react-bootstrap";
 
 export function BoardLayout() {
   return (
     <div>
-      <h3>HOME</h3>
-
       <Carousel className="mb-5">
         <Carousel.Item>
           <img
@@ -42,6 +40,28 @@ export function BoardLayout() {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
+
+      <Tabs defaultActiveKey="home" className="my-4">
+        <Tab eventKey="notice" title="공지사항">
+          <p>뭐뭐뭐</p>
+        </Tab>
+        <Tab eventKey="new" title="최신글">
+          <p>뫄뫄뫄</p>
+        </Tab>
+      </Tabs>
+
+      <Accordion defaultActiveKey="0" className="my-3">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>자주 묻는 질문</Accordion.Header>
+          <Accordion.Body>
+            React-Bootstrap은 Bootstrap 컴포넌트를 React 방식으로 제공합니다.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>사용법은 어렵나요?</Accordion.Header>
+          <Accordion.Body>아주 쉽습니다. 그대로 쓰면 돼요.</Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
   );
 }
